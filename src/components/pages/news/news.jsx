2a4c71fe35d988dd57f25b1f07ca53a25d98e874
Dmitry Seregin <dimenerDS@gmail.com>
2025-10-03 22:14:47 +0400

    Add date sorting to the news page.

--- src/components/pages/news/news.jsx
+++ src/components/pages/news/news.jsx
@@ -1,20 +1,46 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import styles from "./news.module.css";
 import { fetchNews } from "../../../thunk-action/fetch-news";
+
+import styles from "./news.module.css";
 
 export const News = () => {
   const news = useSelector((state) => state.news.news);
   const dispatch = useDispatch();
 
+  const [sortedNews, setSortedNews] = useState(news);
+  const [isSorted, setIsSorted] = useState(false);
+
   useEffect(() => {
     dispatch(fetchNews());
   }, [dispatch]);
 
+  useEffect(() => {
+    setSortedNews(news);
+  }, [news]);
+
+  const sortNewsDate = () => {
+    if (!isSorted) {
+      const newSort = sortedNews
+        .slice()
+        .sort((a, b) => new Date(b.published_at) - new Date(a.published_at));
+      setSortedNews(newSort);
+      setIsSorted(true);
+    } else {
+      setSortedNews(news);
+      setIsSorted(false);
+    }
+  };
+
   return (
     <div className={styles.newsContainer}>
-      {news.map(({ id, title, content, published_at }) => (
+      <button onClick={sortNewsDate}>
+        <i className="fa fa-sort fa-2x" aria-hidden="true">
+          Сортировка по дате
+        </i>
+      </button>
+      {sortedNews.map(({ id, title, content, published_at }) => (
         <article key={id} className={styles.article}>
           <div className={styles.title}>
             <h3>{title}</h3>
